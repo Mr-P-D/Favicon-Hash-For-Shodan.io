c4b5a35b18f8bf64a5c2bdb6430cb54680febc38
matteounitn <32160291+matteounitn@users.noreply.github.com>
2022-08-16 10:28:53 +0200

    Added requests as dependency and URL schema checks

--- get_favicon_hash.py
+++ get_favicon_hash.py
@@ -1,9 +1,11 @@
 import requests,mmh3,base64
 inp = input("Enter Favicon URL to get the mmh3-HASH: ") 
+#check if the URL has a generic schema.
+#Since we don't know the schema, we will check for the presence of :// in the URL.
+while inp.find("://") == -1:
+    inp = input("Enter a valid URL (with schema): ")
 response = requests.get(inp)
 favicon = base64.encodebytes(response.content)
 hash = mmh3.hash(favicon)
 print (hash)
 print ("Now Use this on Shodan For Searching,http.favicon.hash:" + str(hash))
-
-	
